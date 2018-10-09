4e7c455a9ea446295fc90c868efdc07800c1b8e8
Dmitry Chistik <chistik@cetis.ru>
2018-10-10 01:36:12 +0300

    was added files for BinaryTree

---
+++ src/js/BinaryTree.js
@@ -0,0 +1,168 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export default class BinaryTree {
+  constructor(comparator = cmp) {
+    this.comparator = comparator;
+    this.root = null;
+  }
+
+  contains(data) {
+    let found = false;
+    let node = this.root;
+
+    while (!found && node) {
+      if (this.comparator(data, node.data)) {
+        node = node.left;
+      } else if (data > node.data) {
+        node = node.right;
+      } else {
+        found = true;
+      }
+    }
+
+    return found;
+  };
+
+  insert(str) {
+    const newNode = new Node(str);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (this.contains(newNode.data)) {
+      throw new SyntaxError(`This tree already contains '${newNode.data}'`);
+    } else {
+      if (this.comparator(newNode.data, node.data)) {
+        if (node.left === null) {
+          node.left = newNode;
+        } else {
+          this.insertNode(node.left, newNode);
+        }
+      } else {
+        if (node.right === null) {
+          node.right = newNode;
+        } else {
+          this.insertNode(node.right, newNode);
+        }
+      }
+    }
+
+  }
+
+  remove(str) {
+    this.root = this.removeNode(this.root, str);
+  }
+
+  removeNode(node, key) {
+
+    if (!this.contains(key)) {
+      throw new SyntaxError(`This tree does not contain '${key}'`);
+    }
+
+    if (node === null) {
+      return null;
+    } else if (this.comparator(key.data, node.data)) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.data) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      var aux = this.findMinNode(node.right);
+      node.data = aux.data;
+
+      node.right = this.removeNode(node.right, aux.data);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null)
+      return node;
+    else
+      return this.findMinNode(node.left);
+  }
+
+  height() {
+    let height = 0;
+    let node = this.root;
+    let start = 0;
+
+    function traverse(node, depth) {
+      if (!node) return null;
+
+      if (node) {
+        depth++;
+        height = depth > height ? depth : height;
+        traverse(node.left, depth);
+        traverse(node.right, depth);
+      }
+
+    };
+
+    traverse(node, start);
+
+    return height;
+  }
+
+  getNode(process) {
+
+    function inOrder(node) {
+      if (node) {
+        if (node.left !== null) {
+          inOrder(node.left);
+        }
+
+        // Call the process method on this node
+        process.call(this, node);
+
+        if (node.right !== null) {
+          inOrder(node.right);
+        }
+      }
+    }
+
+    inOrder(this.root);
+  }
+
+  toArray() {
+    let arr = [];
+    let node = this.root;
+
+    this.getNode(function(node) {
+      arr.push(node.data);
+    });
+
+    return arr;
+  }
+
+};
+
+let cmp = function(str1, str2) {
+  return str1 < str2;
+};
---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+// import sayHello from './lib/SayHello';
+import BinaryTree from './BinaryTree';
+
+let tree = new BinaryTree();
+
+tree.insert('b');
+tree.insert('a');
+tree.insert('c');
+console.log(tree.height()); //2
+console.log(tree.toArray()); // ['a','b','c']
+
+tree.remove('b');
+tree.insert('b');
+console.log(tree.height()); // 3
+console.log(tree.toArray()); // ['a','b','c']
+
+tree.insert('z');
+console.log(tree.height()); //3
+console.log(tree.toArray()); // ['a','b','c','z']
+
+tree.insert('y');
+tree.insert('x');
+console.log(tree.height()); // 4
+console.log(tree.toArray()); // ['a','b','c','x','y','z']
+
+try {
+  tree.insert('x');
+} catch({message}) {
+  console.log(message); // This tree already contains 'x'
+}
+
+try {
+  console.log('start');
+  tree.remove('x');
+  tree.remove('x');
+  console.log('end');
+} catch({message}) {
+  console.log(message); // This tree does not contain 'x'
+}
+
+tree = new BinaryTree();
+tree.insert('a');
+tree.insert('b');
+tree.insert('c');
+tree.insert('d');
+tree.insert('e');
+
+console.log(tree.height()); // 5
+console.log(tree.toArray()); // ['a','b','c','d','e']
+
+tree.remove('b');
+tree.remove('a');
+tree.remove('c');
+tree.remove('d');
+tree.remove('e');
+
+console.log(tree.height()); // 0
+console.log(tree.toArray()); // []
+
+// sayHello();
